859b6b8367090ca55049f2d347dee37013db835c
Eric Basti <ericjbasti@gmail.com>
2016-04-29 15:32:42 -0400

    complete rewrite.

--- simpleTemplate.js
+++ simpleTemplate.js
@@ -1,71 +1,138 @@
-var SimpleTemplate={
-	ver:'1.2',
-	template:{}
+var SimpleTemplate = {
+	ver: '2.0',
+	template: {},
+	regex: /[{][@](.*?)[}]/g,
+	functions: {
+		toLowerCase: function(a) {
+			a = a + '';
+			return a.toLowerCase();
+		},
+		toUpperCase: function(a) {
+			a = a + '';
+			return a.toUpperCase();
+		},
+		firstChar: function(a) {
+			a = a + '';
+			return a[0];
+		},
+		underscores: function(a) {
+			return a.split(' ').join('_');
+		},
+		removeUnderscores: function(a) {
+			return a.split('_').join(' ');
+		},
+		checked: function(a) {
+			if (a) {
+				return 'checked';
+			} else {
+				return;
+			}
+		},
+		define: function(a) {
+			return a || ''
+		}
+	}
 };
 
-SimpleTemplate._fill = function(who,where,props){
-	var temp=SimpleTemplate.template[who] || who;
-	var identifier;
-	for (var i in where){
-		if(typeof(where[i])=='object'){
-			for (var j in where[i]){
-				identifier = new RegExp("\\%" + i + "."+j+"\\%", "g");
-				temp=temp.replace(identifier,where[i][j]);
-			}
-		}else{
-			var sim=where[i];
-			if(typeof(sim)=='boolean' || typeof(sim)=='number') sim.toString(); // convert booleans & numbers to strings for keying
-			var simCheck=sim;
-			identifier = new RegExp("\\%" + i + "\\%", "g");
-			if(simCheck=='Yes' || simCheck=='true' || simCheck== 'false' || simCheck=='No'){
-				if(props && props[i]){
-					temp=temp.replace(identifier,props[i][simCheck]);
-				}
-			}else{
-				temp=temp.replace(identifier,where[i]);
-			}
-		}
-	}
-	return temp;
-}
-
-SimpleTemplate.fill = function(who, where, props){
-	var temp = [];
-	if(where.length && typeof(where)!='string'){
-		if(where.length){
-			for (var i= 0; i!= where.length; i++){
-				temp.push(SimpleTemplate._fill(who,where[i],props));
-			}
-		}else{
-			for (var j in where){
-				var that = {'this':j};
-				for (var k in where[j]){
-					that[k]=where[j][k];
-				}
-				temp.push(SimpleTemplate._fill(who,that,props));
-			}
-		}
-	}else{
-		temp.push(SimpleTemplate._fill(who,where,props));
-	}
-	return(temp.join(''));
-}
-
-SimpleTemplate.loadTemplate = function(who){
-	if(document.getElementById(who)){
-		SimpleTemplate.template[who]=document.getElementById(who).innerHTML;
-	}else{
-		console.log('failed to load template ['+who+']')
+SimpleTemplate.loadTemplate = function(who) {
+	if (document.getElementById(who)) {
+		SimpleTemplate.template[who] = document.getElementById(who).innerHTML;
+	} else {
+		console.log('failed to load template [' + who + ']')
 	}
 }
 
-SimpleTemplate.loadTemplates = function(){
+SimpleTemplate.buildTemplateFromString = function(name, str) {
+	SimpleTemplate.template[name] = {
+		markup: str,
+		tags: null
+	}
+	SimpleTemplate.template[name].tags = SimpleTemplate.getTags(str);
+}
+
+SimpleTemplate.addFunction = function(name, callback) {
+	this.functions[name] = callback;
+}
+
+
+SimpleTemplate.loadTemplates = function() {
 	var temp = document.getElementsByTagName('script');
-	for (var i=0;i!=temp.length;i++){
-		if(temp[i].type=='simple/template'){
+	for (var i = 0; i != temp.length; i++) {
+		if (temp[i].type == 'simple/template') {
 			var who = temp[i].id;
-			SimpleTemplate.template[who]=temp[i].innerHTML;
+			var markup = temp[i].innerHTML
+			SimpleTemplate.template[who] = {
+				markup: markup,
+				tags: null
+			}
+			SimpleTemplate.template[who].tags = SimpleTemplate.getTags(markup);
 		}
 	}
 }
+
+SimpleTemplate.getTags = function(a) {
+	var found = a.match(SimpleTemplate.regex);
+	var tags = {};
+	for (var i in found) {
+		var temp = found[i];
+		var prop = null;
+		var func = null;
+
+		// We need to remove the template tags from our properties.
+		temp = temp.replace('{@', '').replace('}', '');
+
+		// We'll start at the very end of the string and look for a function call.
+		func = temp.split(',')[1];
+		if (func) { // If we find one, we save it and remove it from temp.
+			temp = temp.split(',')[0].trim();
+		}
+
+		// Now we look for a propery
+		prop = temp.split('.')[1];
+		if (prop) { // If we find one, we save it and remove it from temp.
+			temp = temp.split('.')[0];
+		}
+
+		tags[found[i]] = {
+			prop: prop,
+			func: func,
+			tag: temp
+		}
+	}
+	return (tags);
+}
+
+SimpleTemplate.fill = function(template, obj) {
+	var template = this.template[template];
+	var output = template.markup;
+	var identifier, value;
+	for (var i in template.tags) {
+		identifier = new RegExp("\\" + i, "g");
+		var result = obj[template.tags[i].tag] || '';
+		if (template.tags[i].prop) {
+			try {
+				result = result[template.tags[i].prop];
+			} catch (e) {
+				console.log('SimpleTemplate (' + result + '): ' + e.message);
+			}
+		}
+		if (template.tags[i].func) {
+			try {
+				result = SimpleTemplate.functions[template.tags[i].func](result);
+			} catch (e) {
+				console.log('SimpleTemplate (' + result + '): ' + e.message);
+			}
+		} else {
+			try {
+				result = SimpleTemplate.functions.define(result);
+			} catch (e) {
+				console.log('SimpleTemplate (' + result + '): ' + e.message);
+			}
+		}
+
+		output = output.replace(identifier, result);
+	}
+	return (output);
+}
+
 SimpleTemplate.loadTemplates();
